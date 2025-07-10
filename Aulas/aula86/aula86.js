b88a12af32c1cff3dfabfac7aef0c78c3db59672
Gabriel Santos <gabrielhenriquex1566@gmail.com>
2025-07-10 11:21:53 -0300

    Update aula86.js

--- Aulas/aula86/aula86.js
+++ Aulas/aula86/aula86.js
@@ -6,10 +6,26 @@ function rand(min, max){
 
 function esperaAi(msg, tempo) {
   return new Promise((resolve, reject)=> {
+    if(typeof msg !== 'string') reject(new Error('ERRO'));
     setTimeout(() => {
-      console.log(msg);
-      resolve();
+      resolve(msg);
     }, tempo);
   });
-
 }
+
+esperaAi('Conexão com o BD', rand(1, 3))
+.then(resposta => {
+  console.log(resposta);
+  return esperaAi('Buscando dados na Base', rand(1, 3));
+})
+.then(resposta => {
+  console.log(resposta);
+  return esperaAi(2222, rand(1,3));
+}).then(resposta => {
+  console.log(resposta)
+}).then(() => {
+  console.log('Exibe dados na tela')
+})
+.catch(e => {console.log('Error: ', e)});
+
+console.log('Isso aqui será exibido antes de qualquer promisse.')
