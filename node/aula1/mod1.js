7e08195c723b629e19ca4000857fafaf3603544e
Gabriel Santos <gabrielhenriquex1566@gmail.com>
2025-07-28 12:46:43 -0300

    exports

---
+++ node/aula1/mod1.js
@@ -0,0 +1,29 @@
+class Pessoa {
+  constructor(nome) {
+    this.nome = nome;
+  }
+}
+exports.Pessoa = Pessoa
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const nome = 'Henrique';
+// const sobrenome = 'Santos';
+
+// const falaNome = () => nome + ' ' + sobrenome;
+
+// exports.nome = nome;
+// exports.sobrenome = sobrenome;
+// exports.falaNome = falaNome;
