d9fd80ee2428436c5efe3cae2425c45e454c6599
Gbrx Dev <gabrielhenriquex1566@gmail.com>
2026-03-17 18:34:19 -0300

    execp

---
+++ Aulas/Exp-reg/base.js
@@ -0,0 +1,11 @@
+const texto = `Gabriel trouxe flores para sua amada namorada em 10 de janeiro de 1970, Maria era o nome dela.
+
+💡
+
+Foi um ano excelente na vida de gabriel. Teve 5 filhos, todos adultos atualmente. maria, hoje sua esposa, ainda faz aquele café com pão de queijo nas tardes de domingo. Também né! Sendo a boa mineira que é, nunca esquece seu famoso pão de queijo.
+Não canso de ouvir a Maria:
+"Gaaaabrieeeeeel, o café tá prontinho aqui. Veeemm"!`
+
+module.exports = { 
+  texto
+}
